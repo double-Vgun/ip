bc20b5e039487e5659a8815c8e2ba23bcda1a819
Vivekan K <vivekankathir@gmail.com>
2026-08-26 23:45:03 +0800

    Add,List

--- src/main/java/ATHENA.java
+++ src/main/java/ATHENA.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ATHENA {
-    public static void horlines(){
+    public static void horizontal_lines(){
         System.out.println("____________________________________________________________");
     }
     public static void Greet(){
-        horlines();
+        horizontal_lines();
         String banner = " █████╗ ████████╗██╗  ██╗███████╗███╗   ██╗ █████╗\n"
                 + "██╔══██╗╚══██╔══╝██║  ██║██╔════╝████╗  ██║██╔══██╗\n"
                 + "███████║   ██║   ███████║█████╗  ██╔██╗ ██║███████║\n"
@@ -15,18 +17,19 @@ public class ATHENA {
         System.out.println(banner);
         System.out.println("Hello! I'm ATHENA.");
         System.out.println("What can I do for you?");
-        horlines();
+        horizontal_lines();
 
     }
     public static void Exit(){
-        horlines();
+        horizontal_lines();
         System.out.println("Bye. Hope to see you again soon!");
-        horlines();
+        horizontal_lines();
     }
 
     public static void main(String[] args) {
+        String[] store_list = new String[100];
+        int numberofentries =0;
         Greet();
-
         Scanner in = new Scanner(System.in);
         while (in.hasNextLine()) {
             String command = in.nextLine();
@@ -36,9 +39,20 @@ public class ATHENA {
                 break;
             }
 
-            horlines();
-            System.out.println(" " + command);
-            horlines();
+            horizontal_lines();
+            switch (command) {
+                case "list":
+                    for (int i = 0; i < numberofentries; i++) {
+                        System.out.println((i + 1) + ". " + store_list[i]);
+                    }
+                    break;
+                default:
+                    store_list[numberofentries] = command;
+                    numberofentries++;
+                    System.out.println("added: " + command);
+                    break;
+            }
+            horizontal_lines();
         }
         in.close();
     }
